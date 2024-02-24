f5a870310ada335c964812287ae8e418b35063b3
Birendra <bbhandari19982020@gmail.com>
2024-02-24 15:59:01 +0530

    added http methods and models

---
+++ src/main/java/com/restapi/restdemo/controller/CloudVendorAPIService.java
@@ -0,0 +1,38 @@
+package com.restapi.restdemo.controller;
+
+import com.restapi.restdemo.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+
+        return cloudVendor;
+//        return new CloudVendor("c1", "Vendor 1", "Address 1", "xxxxx");
+
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+
+}
